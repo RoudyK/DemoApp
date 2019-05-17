543928f8ec1d23cd111b884043b7696c1a97d8aa
roudipunters <roudi@punters.com.au>
2019-05-17 20:09:04 +1000

    Fix some ui bugs

--- app/src/main/java/com/demo/roudykk/demoapp/extensions/ViewUtil.kt
+++ app/src/main/java/com/demo/roudykk/demoapp/extensions/ViewUtil.kt
@@ -12,11 +12,3 @@ fun RatingBar.applyTheme() {
     val stars = progressDrawable as LayerDrawable
     stars.getDrawable(0).setColorFilter(ContextCompat.getColor(context, R.color.colorRating), PorterDuff.Mode.SRC_ATOP)
 }
-
-fun AppCompatActivity.applyPushAnimation() {
-    overridePendingTransition(R.anim.fade_in_left, R.anim.fade_out_left)
-}
-
-fun AppCompatActivity.applyPopAnimation() {
-    overridePendingTransition(R.anim.fade_in_right, R.anim.fade_out_right)
-}
--- app/src/main/java/com/demo/roudykk/demoapp/ui/activity/BaseActivity.kt
+++ app/src/main/java/com/demo/roudykk/demoapp/ui/activity/BaseActivity.kt
@@ -1,19 +1,13 @@
 package com.demo.roudykk.demoapp.ui.activity
 
-import android.content.Context
-import android.content.Intent
 import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.ContextCompat
 import com.demo.roudykk.demoapp.R
 import com.demo.roudykk.demoapp.db.PreferenceRepo
-import com.demo.roudykk.demoapp.extensions.applyPopAnimation
-import com.demo.roudykk.demoapp.extensions.applyPushAnimation
-import io.github.inflationx.viewpump.ViewPumpContextWrapper
 
 abstract class BaseActivity : AppCompatActivity() {
 
@@ -22,9 +16,9 @@ abstract class BaseActivity : AppCompatActivity() {
         if (PreferenceManager.getDefaultSharedPreferences(this)
                         .getBoolean(PreferenceRepo.PREFERENCE_DARK_THEME, false)) {
             setTheme(R.style.AppTheme_Dark)
-            delegate.setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
         } else {
-            delegate.setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
         }
         super.onCreate(savedInstanceState)
     }
@@ -36,15 +30,5 @@ abstract class BaseActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        applyPopAnimation()
-    }
-
-    override fun startActivity(intent: Intent?) {
-        super.startActivity(intent)
-        applyPushAnimation()
     }
 }
